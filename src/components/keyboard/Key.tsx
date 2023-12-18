363a5e6806f8ba27892310d95799c5874bcecbf3
andrew <andrew@mccllstr.com>
2023-12-17 22:38:15 -0500

    Update Key.tsx

--- src/components/keyboard/Key.tsx
+++ src/components/keyboard/Key.tsx
@@ -33,13 +33,13 @@ export const Key = ({
       'bg-slate-200 dark:bg-slate-600 hover:bg-slate-300 active:bg-slate-400':
         !status,
       'bg-slate-400 dark:bg-slate-800 text-white': status === 'absent',
-      'bg-orange-500 hover:bg-orange-600 active:bg-orange-700 text-white':
+      'bg-yellow-500 hover:bg-yellow-600 active:bg-yellow-700 text-white':
         status === 'correct' && isHighContrast,
       'bg-cyan-500 hover:bg-cyan-600 active:bg-cyan-700 text-white':
         status === 'present' && isHighContrast,
-      'bg-lime-400 hover:bg-lime-500 active:bg-lime-600 text-white':
+      'bg-green-400 hover:bg-green-500 active:bg-green-600 text-white':
         status === 'correct' && !isHighContrast,
-      'bg-orange-500 hover:bg-orange-600 active:bg-orange-700 text-white':
+      'bg-yellow-500 hover:bg-yellow-600 active:bg-yellow-700 text-white':
         status === 'present' && !isHighContrast,
     }
   )
